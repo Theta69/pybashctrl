c9900a21f302bc24e5163af0051062cbc8089231
Theta <78639181+Theta69@users.noreply.github.com>
2023-02-16 11:00:14 +0200

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,10 +1,4 @@
-while getopts u:a:f: flag
-do
-    case "${flag}" in
-        n) n=${OPTARG};;
-    esac
-done
 cd /Documents
-wget https://github.com/Theta69/pybashctrl/blob/main/remote.sh -O ./remote.sh
-cmd='ssh root@10.10.10.$n \'bash -s\' < ./remote.sh'
+wget https://raw.githubusercontent.com/Theta69/pybashctrl/main/remote.sh -O ./remote.sh
+cmd='ssh root@10.10.10.$1 \'bash -s\' < ./remote.sh'
 eval "$cmd"
