f359db8d074566c06069f3a0d7ab8eafaacc0034
Theta <78639181+Theta69@users.noreply.github.com>
2023-02-16 10:22:45 +0200

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 cd /Documents
-wget https://github.com/Theta69/pybashctrl/blob/main/remote.sh -O ./toRun.sh
-cmd='ssh root@10.10.10.$1 \'bash -s\' < ./toRun.sh'
+wget https://github.com/Theta69/pybashctrl/blob/main/remote.sh -O ./remote.sh
+cmd='ssh root@10.10.10.$1 \'bash -s\' < ./remote.sh'
 eval "$cmd"
